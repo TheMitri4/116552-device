91e43916a6efcb60d78ffedba73ce05887b99601
TheMitri4 <DimaMK98@gmail.com>
2018-04-02 22:00:12 +0300

    Оживление слайдера

--- js/js.js
+++ js/js.js
@@ -7,7 +7,43 @@ var writeusclose = writeuspopup.querySelector(".close");
 var form = writeuspopup.querySelector("form");
 var username = writeuspopup.querySelector("[name=name]");
 var email = writeuspopup.querySelector("[name=email]");
-		
+var sliderDotsList = document.querySelectorAll(".dots a");
+var sliderItemList = document.querySelectorAll(".slider-item");
+var servicesLinkList = document.querySelectorAll(".services-navigation a");
+var servicesItemList = document.querySelectorAll(".services-item");
+
+for (var i = 0; i < sliderDotsList.length; i++) {
+	changeSlide(i);
+}
+
+function changeSlide(i) {
+	sliderDotsList[i].addEventListener("click", function (evt) {
+		evt.preventDefault();
+		if (!this.classList.contains("active")) {
+			document.querySelector(".dots .active").classList.remove("active");
+			this.classList.add("active");
+			document.querySelector(".slider-item.active").classList.remove("active");
+			sliderItemList[i].classList.add("active");
+		}
+	});
+}
+
+for(var i=0; i < servicesLinkList.length; i++) {
+	changeServices (i);
+}
+
+function changeServices(i) {
+	servicesLinkList[i].addEventListener("click", function(evt) {
+		evt.preventDefault();
+		if (!this.classList.contains("services-selected")){
+			document.querySelector(".services-navigation .services-selected").classList.remove("services-selected");
+			this.classList.add("services-selected");
+			document.querySelector(".services-item.active").classList.remove("active");
+			servicesItemList[i].classList.add("active");
+		}
+	})
+}
+
 maplink.addEventListener("click", function (evt) {
 	"use strict";
 	evt.preventDefault();
@@ -42,12 +78,16 @@ form.addEventListener("submit", function (evt) {
 		if (!username.value) {
 			evt.preventDefault();
 			username.classList.remove("modal-error");
-			username.classList.add("modal-error");
+			setTimeout(function () {
+				username.classList.add("modal-error");
+			}, 10);
 		}
 		if (!email.value) {
 			evt.preventDefault();
 			email.classList.remove("modal-error");
-			email.classList.add("modal-error");
+			setTimeout(function () {
+				email.classList.add("modal-error");
+			}, 10);
 		}
 	}
 });
